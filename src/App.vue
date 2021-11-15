ce2defe3127ca03ff92da04bfced7fd2c694523b
NicolaL95 <nicola.lilla@gmail.com>
2021-11-15 12:14:15 +0100

    Testing

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <Test
+      v-for="post in posts"
+      v-bind:key="post.id"
+      v-bind:title="post.title"
+    ></Test>
+  </div>
+</template>
+
+<script>
+import Test from "./components/Test.vue";
+
+Vue.component("app-user", {
+  data: function () {
+    return {
+      users: [
+        {
+          username: "max",
+        },
+        {
+          username: "max",
+        },
+        {
+          username: "max",
+        },
+      ],
+    };
+  },
+  template:
+    '<div class="user" v-for="user in users"><p>username: {{user.username }} </p></div>',
+});
+
+export default {
+  name: "App",
+  components: {
+    Test,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue" },
+        { id: 2, title: "Blogging with Vue" },
+        { id: 3, title: "Why Vue is so fun" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button>
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
